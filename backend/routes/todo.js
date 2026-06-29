4d0cbe82ad84e2043427aeb6723c601d3cbd763b
FahimHasan462 <fahimhasan462@gmail.com>
2026-06-29 15:48:23 +0600

    still isseue

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -61,6 +61,10 @@ router.get("/overview", managementOnly, async (req, res) => {
     const configs = dbConfigs.length > 0 ? dbConfigs : SHEET_CONFIG_SEED;
     const projects = {};
 
+    for (const config of configs) {
+      if (!projects[config.project]) projects[config.project] = {};
+    }
+
     await Promise.all(
       configs.map(async (config) => {
         try {
@@ -84,10 +88,6 @@ router.get("/overview", managementOnly, async (req, res) => {
           }
 
           artists.sort((a, b) => a.name.localeCompare(b.name));
-
-          if (!projects[config.project]) {
-            projects[config.project] = {};
-          }
 
           projects[config.project][config.episode] = {
             statuses: mergeStatusColumns(artists.map((artist) => artist.statuses)),
@@ -124,13 +124,16 @@ router.get("/", async (req, res) => {
     const configs = dbConfigs.length > 0 ? dbConfigs : SHEET_CONFIG_SEED;
     const grouped = {};
 
+    for (const config of configs) {
+      if (!grouped[config.project]) grouped[config.project] = {};
+    }
+
     await Promise.all(
-     configs.map(async (config) => {
-  const cuts = await fetchCutsForArtist(config.sheetUrl, sheetName);
-  if (cuts.length === 0) return;
-  if (!grouped[config.project]) grouped[config.project] = {};
-  grouped[config.project][config.episode] = cuts;
-}),
+      configs.map(async (config) => {
+        const cuts = await fetchCutsForArtist(config.sheetUrl, sheetName);
+        if (cuts.length === 0) return;
+        grouped[config.project][config.episode] = cuts;
+      }),
     );
 
     res.json(grouped);
